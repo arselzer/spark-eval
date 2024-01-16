5483d2e65620276107e18da6816c82d81d6f31a5
Alexander Selzer <aselzer3@gmail.com>
2024-01-16 14:03:21 +0000

    simplify loading of JOB data

--- benchmark/install-pyspark.sh
+++ benchmark/install-pyspark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f  pyspark-3.5.0.dev0.tar.gz ]; then
-	wget https://github.com/arselzer/spark/releases/download/v1/pyspark-3.5.0.dev0.tar.gz
+	wget https://github.com/arselzer/spark/releases/download/v3/pyspark-3.5.0.dev0.tar.gz
 fi
 
 pip3 install pyspark-3.5.0.dev0.tar.gz
